0935ad7bd2d209a88f0c56d57699250a2128c725
Tomasz Libich <t.libich@gmail.com>
2025-12-30 22:08:42 +0000

    Update Caddyfile to final version and add deploy-all.sh script

---
+++ scripts/deploy-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Always run relative to this script’s location
+cd "$(dirname "$0")"
+
+echo "Deploying lbt-auth-api..."
+cd ../../lbt-auth-api
+git pull origin master
+docker compose up -d --build
+
+echo "Deploying lbt-gateway..."
+cd ../lbt-gateway
+git pull origin master
+docker compose up -d --build
+
+echo "Deploying lbt-reverse-proxy..."
+cd ../lbt-reverse-proxy
+git pull origin master
+docker compose up -d --build
+
+echo "Done."
